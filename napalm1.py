c5adc129c9e0f54161c15f214bea450cccc522e3
aboyum <36292396+aboyum@users.noreply.github.com>
2020-01-14 20:19:29 +0100

    update1

---
+++ napalm1.py
@@ -0,0 +1,14 @@
+from napalm import get_network_driver
+import json
+
+driver_ios = get_network_driver('ios')
+sw1 = driver_ios('10.0.0.1', 'user', 'cisco')
+sw1.open()
+
+#Get list of interfaces and IOS version and such
+sw1_output = sw1.get_facts()
+print json.dumps(sw1_output, indent=4))
+
+#Get more info of interfaces
+sw1_output = sw1.get_interfaces()
+print json.dumps(sw1_output, indent=4))
