9056cf75794539c42603825b9411bfc0abecf52f
aboyum <36292396+aboyum@users.noreply.github.com>
2020-01-14 20:46:16 +0100

    update4

--- napalm1.py
+++ napalm1.py
@@ -2,7 +2,7 @@ from napalm import get_network_driver
 import json
 
 driver_ios = get_network_driver('ios')
-sw1 = driver_ios('10.0.0.1', 'user', 'cisco')
+sw1 = driver_ios('10.0.0.1', 'user', 'pasword')
 sw1.open()
 
 #Get list of interfaces and IOS version and such
--- napalm_config.py
+++ napalm_config.py
@@ -2,5 +2,20 @@ from napalm import get_network_driver
 import json
 
 driver_ios = get_network_driver('ios')
-sw1 = driver_ios('10.0.0.1', 'user', 'cisco')
+sw1 = driver_ios('10.0.0.1', 'user', 'password')
 sw1.open()
+
+print ('Accessing')
+sw1.load_merge_candidate(filename='newconfig.cfg')
+
+diffs = sw1.compare_config()
+if len(diffs) > 0:
+    print(diffs)
+    sw1.commit_config()
+else:
+    print('No changes required!')
+    sw1.discard_config
+sw2.close()
+
+# Documentation about config changes:
+# https://napalm.readthedocs.io/en/latest/support/index.html
